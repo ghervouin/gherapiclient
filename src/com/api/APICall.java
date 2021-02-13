27f4d2285fb3a5d17cbdf600a782e1e587985cd5
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-13 13:00:40 +0100

    Update APICall.java

--- src/com/api/APICall.java
+++ src/com/api/APICall.java
@@ -7,9 +7,15 @@ import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 
+import com.api.TestURL.TestStatistics;
+
+import java.nio.ByteBuffer;
+
 public class APICall {
 
-	static File OUTPUT_LOCATION = new File("output");
+	static final File OUTPUT_LOCATION = new File("output");
+	static final int NB_TRIAL = 25;
+	
 	public static void main(String[] args) throws Exception {
 
 		TestURL testShort1 = new TestURL("6KB-BACKEND", new URL("https://witty-wave-08c1e3003.azurestaticapps.net/api/short"));
@@ -27,17 +33,11 @@ public class APICall {
 		TestURL testBig3 = new TestURL("8MB-API-AZURE_EU_NORTH", new URL("https://gherapi.azure-api.net/big"));
 		TestURL testBig4 = new TestURL("8MB-API-AZURE_EU_WEST", new URL("https://gherapiwest.azure-api.net/big"));
 
-		/** dry-run **/
-		runTest(testShort1, 4);
-		runTest(testShort2, 4);
-		runTest(testShort3, 4);
-		runTest(testShort4, 4);
-
 		/** real-run **/
-		runTest(testShort1, 20);
-		runTest(testShort2, 20);
-		runTest(testShort3, 20);
-		runTest(testShort4, 20);
+		runTest(testShort1, NB_TRIAL);
+		runTest(testShort2, NB_TRIAL);
+		runTest(testShort3, NB_TRIAL);
+		runTest(testShort4, NB_TRIAL);
 
 		/** results **/
 		System.out.println("---------------");
@@ -48,18 +48,13 @@ public class APICall {
 		System.out.println("---------------");
 		
 		exporCSV("testShort.csv", testShort1, testShort2, testShort3, testShort4);
-
-		/** dry-run **/
-		runTest(testMessage1, 4);
-		runTest(testMessage2, 4);
-		runTest(testMessage3, 4);
-		runTest(testMessage4, 4);
+		exporSummaryCSV("testShortSummary.csv", testShort1, testShort2, testShort3, testShort4);
 
 		/** real-run **/
-		runTest(testMessage1, 20);
-		runTest(testMessage2, 20);
-		runTest(testMessage3, 20);
-		runTest(testMessage4, 20);
+		runTest(testMessage1, NB_TRIAL);
+		runTest(testMessage2, NB_TRIAL);
+		runTest(testMessage3, NB_TRIAL);
+		runTest(testMessage4, NB_TRIAL);
 
 		/** results **/
 		System.out.println("---------------");
@@ -70,20 +65,15 @@ public class APICall {
 		System.out.println("---------------");
 		
 		exporCSV("testMessage.csv", testMessage1, testMessage2, testMessage3, testMessage4);
-
-		/** dry-run **/
-		runTest(testBig1, 4);
-		runTest(testBig2, 4);
-		runTest(testBig3, 4);
-		runTest(testBig4, 4);
+		exporSummaryCSV("testMessageSummary.csv", testMessage1, testMessage2, testMessage3, testMessage4);
 
 		/** real-run **/
-		runTest(testBig1, 20);
-		runTest(testBig2, 20);
-		runTest(testBig3, 20);
-		runTest(testBig4, 20);
+		runTest(testBig1, NB_TRIAL);
+		runTest(testBig2, NB_TRIAL);
+		runTest(testBig3, NB_TRIAL);
+		runTest(testBig4, NB_TRIAL);
 
-		/** results **/
+		// /** results **/
 		System.out.println("---------------");
 		System.out.println(testBig1.getPrintableResult());
 		System.out.println(testBig2.getPrintableResult());
@@ -92,55 +82,105 @@ public class APICall {
 		System.out.println("---------------");
 		
 		exporCSV("testBig.csv", testBig1, testBig2, testBig3, testBig4);
+		exporSummaryCSV("testBigSummary.csv", testBig1, testBig2, testBig3, testBig4);
 	}
 
 	static void runTest(TestURL test, int nbtrials) throws Exception {
-		File testOutput = new File(OUTPUT_LOCATION, test.id);
-		testOutput.delete();
-		testOutput.mkdir();
-
 		long[] c = new long[nbtrials];
-		for (int i=0; i<nbtrials; i++) c[i] = readAndStore(test, i, testOutput);
-
-		test.results = c;
+		for (int i=0; i<nbtrials; i++) {
+			// c[i] = readAndStore(test, i);
+			c[i] = readAndForget(test, i);
+		}
+		test.setResults(c);
 		System.out.println(test);
 	}
 
-	static long readAndStore(TestURL test, int runId, File outputFolder) throws Exception {
-		long l = System.currentTimeMillis();		
-		ReadableByteChannel readableByteChannel = Channels.newChannel(test.url.openStream());
-		try (FileOutputStream fileOutputStream = new FileOutputStream(new File(outputFolder, test.id+"_"+runId));
-				FileChannel fileChannel = fileOutputStream.getChannel()) {
+	static long readAndStore(TestURL test, int runId) throws Exception {
+		File testOutput = new File(OUTPUT_LOCATION, test.getId());
+		if (testOutput.exists()) {
+			testOutput.delete();
+			testOutput.mkdir();
+		}
+		long l = System.currentTimeMillis();	
+		URL nocacheUrl = new URL(test.getUrl().toString()+"?timestamp="+l);	
+		try (FileOutputStream fileOutputStream = new FileOutputStream(new File(testOutput, test.getId()+"_"+runId));
+				FileChannel fileChannel = fileOutputStream.getChannel();
+				ReadableByteChannel readableByteChannel = Channels.newChannel(nocacheUrl.openStream());) {
 			fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+		}
+		catch(java.net.SocketException e) {
+			System.err.println("Socket exception: " + e);
 		}
 		return (System.currentTimeMillis()-l);
 	}
 
+	static long readAndForget(TestURL test, int runId) throws Exception {
+		long l = System.currentTimeMillis();	
+		URL nocacheUrl = new URL(test.getUrl().toString()+"?timestamp="+l);	
+		try (ReadableByteChannel readableByteChannel = Channels.newChannel(nocacheUrl.openStream())) {
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			while (readableByteChannel.read(buffer) != -1) {
+				buffer.clear();
+			}
+		}
+		catch(java.net.SocketException e) {
+			System.err.println("Socket exception: " + e);
+		}
+		return (System.currentTimeMillis()-l);
+	}
+	
 	static void exporCSV(String filename, TestURL ... testURLs) throws Exception {
 		String DEL = ";";
 
-		try (FileOutputStream fileOutputStream = new FileOutputStream(filename);
-				FileChannel fileChannel = fileOutputStream.getChannel()) {
+		try (FileOutputStream fileOutputStream = new FileOutputStream(
+				new File(OUTPUT_LOCATION, filename))) {
 
 			int trials = 0;
-			String title = "Trial ID"+DEL;
+			String header = "Trial ID"+DEL;
 			for (TestURL testUrl : testURLs) {
-				long[] testUrls = testUrl.results;
+				long[] testUrls = testUrl.getResults();
 				trials = testUrls.length;
-				title += testUrl.id+DEL;
+				header += testUrl.getId()+DEL;
 			}
-			title += "\n";
-			fileOutputStream.write(title.getBytes());
+			header += "\n";
+			fileOutputStream.write(header.getBytes());
 
 			String s = "";
 			for (int i=0; i<trials; i++) {
 				s += "T"+(i+1)+DEL;
 				for (TestURL testUrl : testURLs) {
-					s += testUrl.results[i]+DEL;
+					s += testUrl.getResults()[i]+DEL;
 				}
 				s += "\n";
 			}
 			fileOutputStream.write(s.getBytes());
+		}
+	}
+	
+	static void exporSummaryCSV(String filename, TestURL ... testURLs) throws Exception {
+		String DEL = ";";
+
+		try (FileOutputStream fileOutputStream = new FileOutputStream(
+				new File(OUTPUT_LOCATION, filename))) {
+
+			String header = "ID"+DEL+"CNT"+DEL+"SUM"+DEL+"MIN"+DEL+"MAX"+DEL+"AVG"+DEL+"MED"+DEL+"STD"+"\n";
+			fileOutputStream.write(header.getBytes());
+			
+			for (TestURL t : testURLs) {
+				TestStatistics ts = t.getTestStatistics();
+				String s = "";
+				s += t.getId()+DEL;
+				s += ts.getCount()+DEL;
+				s += ts.getSum()+DEL;
+				s += ts.getMin()+DEL;
+				s += ts.getMax()+DEL;
+				s += ts.getAverage()+DEL;
+				s += ts.getMedian()+DEL;
+				s += ts.getStderr()+DEL;
+				s += "\n";
+				fileOutputStream.write(s.getBytes());
+			}
+			
 		}
 	}
 }
