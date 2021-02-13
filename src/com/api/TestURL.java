b5d5291b725548fa03d514b290a5166ccf77e056
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-13 13:00:56 +0100

    Update TestURL.java

--- src/com/api/TestURL.java
+++ src/com/api/TestURL.java
@@ -5,38 +5,123 @@ import java.util.Arrays;
 import java.util.LongSummaryStatistics;
 
 public class TestURL {
-	String id;
-	URL url;
-	long[] results;
+	private String id;
+	private URL url;
+	private long[] results;
 
 	TestURL(String id, URL url) {
 		this.id = id;
 		this.url = url;
 	}
 
-	String getPrintableResult() {
-		String s = id+"\n";
+	public String getId() {
+		return this.id;
+	}
+	
+	public URL getUrl() {
+		return this.url;
+	}
+	
+	public void setResults(long[] results) {
+		this.results = results;
+	}
+	
+	public long[] getResults() {
+		return this.results;
+	}
+	
+	public TestStatistics getTestStatistics() {
+		TestStatistics stat = new TestStatistics();
+		
 		LongSummaryStatistics s1 = Arrays.stream(results).summaryStatistics();
-		s += "  CNT: "+s1.getCount()+"\n";
-		s += "  SUM: "+s1.getSum()+"\n";
-		s += "  MIN: "+s1.getMin()+"\n";
-		s += "  MAX: "+s1.getMax()+"\n";
-		s += "  AVG: "+(long)s1.getAverage()+"\n";
-		s += "  STD: "+(long)Math.sqrt(variance(results, s1.getAverage()));
-		return s;
+		stat.count = s1.getCount();
+		stat.sum = s1.getSum();
+		stat.min = s1.getMin();
+		stat.max = s1.getMax();
+		stat.average = (long)s1.getAverage();
+		stat.median = (long)median(results);
+		stat.stderr = (long)Math.sqrt(variance(results, s1.getAverage()));
+		return stat;
 	}
 
-	private static double variance(long a[], double average) { 
-    	int n = a.length;
+	String getPrintableResult() {		
+		return id+"\n" + getTestStatistics().toString();
+	}
+
+	private static double variance(long values[], double average) { 
+    	int n = values.length;
         double sqDiff = 0; 
         for (int i = 0; i < n; i++)  
-            sqDiff += (a[i] - average) *  
-                      (a[i] - average); 
-          
+            sqDiff += (values[i] - average) *  
+                      (values[i] - average); 
         return (double)sqDiff / n; 
     }
     
+	private static long median(long[] values) {
+		Arrays.sort(values);
+	    long median;
+	    int totalElements = values.length;
+	    if (totalElements % 2 == 0) {
+	        long sumOfMiddleElements = values[totalElements / 2] +
+	                                  values[totalElements / 2 - 1];
+	        median = ((long) sumOfMiddleElements) / 2;
+	    } else {
+	        median = (long) values[values.length / 2];
+		}
+	    return median;
+	}
+
     public String toString() {
     	return id+":"+Arrays.toString(results);
+    }
+    
+    public class TestStatistics {
+    	private long count;
+    	private long sum;
+    	private long min;
+    	private long max;
+    	private long average;
+    	private long median;
+    	private long stderr;
+
+    	public long getCount() {
+			return count;
+		}
+
+		public long getSum() {
+			return sum;
+		}
+
+		public long getMin() {
+			return min;
+		}
+
+		public long getMax() {
+			return max;
+		}
+
+		public long getAverage() {
+			return average;
+		}
+
+		public long getMedian() {
+			return median;
+		}
+
+		public long getStderr() {
+			return stderr;
+		}
+
+    	public String toString() {
+    		String s = id+"\n";
+    		s += "  CNT: "+count+"\n";
+    		s += "  SUM: "+sum+"\n";
+    		s += "  MIN: "+min+"\n";
+    		s += "  MAX: "+max+"\n";
+    		s += "  AVG: "+average+"\n";
+    		s += "  MED: "+median+"\n";
+    		s += "  STD: "+stderr;
+    		return s;
+    	}
     }
 }
