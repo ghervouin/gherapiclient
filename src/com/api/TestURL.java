ece9fa6714df8c5225b9d9a7c4bba92f16c84c55
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-15 18:46:10 +0100

    Update TestURL.java

--- src/com/api/TestURL.java
+++ src/com/api/TestURL.java
@@ -1,127 +1,156 @@
 package com.api;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.LongSummaryStatistics;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 
-public class TestURL {
-	private String id;
-	private URL url;
-	private long[] results;
+import com.api.TestURL.TestStatistics;
 
-	TestURL(String id, URL url) {
-		this.id = id;
-		this.url = url;
-	}
+import java.nio.ByteBuffer;
 
-	public String getId() {
-		return this.id;
-	}
+public class APICall {
+
+	static final File OUTPUT_LOCATION = new File("output");
+	static final int NB_TRIAL = 20;
 	
-	public URL getUrl() {
-		return this.url;
-	}
-	
-	public void setResults(long[] results) {
-		this.results = results;
-	}
-	
-	public long[] getResults() {
-		return this.results;
-	}
-	
-	public TestStatistics getTestStatistics() {
-		TestStatistics stat = new TestStatistics();
+	public static void main(String[] args) throws Exception {
+
+		if (OUTPUT_LOCATION.exists()) {
+			OUTPUT_LOCATION.delete();
+		}
+		OUTPUT_LOCATION.mkdir();
+
+		TestSuite testSmall = new TestSuite("Small-6KB");
+		testSmall.addTest("BACKEND", new URL("https://witty-wave-08c1e3003.azurestaticapps.net/api/short"));
+		testSmall.addTest("APIGEE_EU", new URL("http://axa-mvp-entity-test.apigee.net/ghertest/short"));
+		testSmall.addTest("AZ_EU_NORTH", new URL("https://gherapi.azure-api.net/short"));
+		testSmall.addTest("AZ_EU_WEST", new URL("https://gherapiwest.azure-api.net/short"));
+
+		TestSuite testMedium = new TestSuite("Medium-2MB");
+		testMedium.addTest("BACKEND", new URL("https://witty-wave-08c1e3003.azurestaticapps.net/api/message"));
+		testMedium.addTest("APIGEE_EU", new URL("http://axa-mvp-entity-test.apigee.net/ghertest/message"));
+		testMedium.addTest("AZ_EU_NORTH", new URL("https://gherapi.azure-api.net/message"));
+		testMedium.addTest("AZ_EU_WEST", new URL("https://gherapiwest.azure-api.net/message"));
+
+		TestSuite testLarge = new TestSuite("Large-8MB");
+		testLarge.addTest("BACKEND", new URL("https://witty-wave-08c1e3003.azurestaticapps.net/api/big"));
+		testLarge.addTest("APIGEE_EU", new URL("http://axa-mvp-entity-test.apigee.net/ghertest/big"));
+		testLarge.addTest("AZ_EU_NORTH", new URL("https://gherapi.azure-api.net/big"));
+		testLarge.addTest("AZ_EU_WEST", new URL("https://gherapiwest.azure-api.net/big"));
+
+		runTest(testSmall, NB_TRIAL);
+		System.out.println(testSmall);
+		exporCSV(testSmall.getId()+".csv", testSmall);
+		exporSummaryCSV(testSmall.getId()+"-Summary.csv", testSmall);
 		
-		LongSummaryStatistics s1 = Arrays.stream(results).summaryStatistics();
-		stat.count = s1.getCount();
-		stat.sum = s1.getSum();
-		stat.min = s1.getMin();
-		stat.max = s1.getMax();
-		stat.average = (long)s1.getAverage();
-		stat.median = (long)median(results);
-		stat.stderr = (long)Math.sqrt(variance(results, s1.getAverage()));
-		return stat;
+		runTest(testMedium, NB_TRIAL);
+		System.out.println(testMedium);
+		exporCSV(testMedium.getId()+".csv", testMedium);
+		exporSummaryCSV(testMedium.getId()+"-Summary.csv", testMedium);
+		
+		runTest(testLarge, NB_TRIAL);
+		System.out.println(testLarge);				
+		exporCSV(testLarge.getId()+".csv", testLarge);
+		exporSummaryCSV(testLarge.getId()+"-Summary.csv", testLarge);
 	}
 
-	String getPrintableResult() {		
-		return id+"\n" + getTestStatistics().toString();
+	static void runTest(TestSuite testSuite, int nbTrials) throws Exception {
+		for (int i=0; i<nbTrials; i++) {
+			for (TestURL test : testSuite.getTests()) {
+				// test.addResult(readAndStore(test, i));
+				test.addResult(readAndForget(test, i));
+			}
+		}
 	}
 
-	private static double variance(long values[], double average) { 
-    	int n = values.length;
-        double sqDiff = 0; 
-        for (int i = 0; i < n; i++)  
-            sqDiff += (values[i] - average) *  
-                      (values[i] - average); 
-        return (double)sqDiff / n; 
-    }
-    
-	private static long median(long[] values) {
-		Arrays.sort(values);
-	    long median;
-	    int totalElements = values.length;
-	    if (totalElements % 2 == 0) {
-	        long sumOfMiddleElements = values[totalElements / 2] +
-	                                  values[totalElements / 2 - 1];
-	        median = ((long) sumOfMiddleElements) / 2;
-	    } else {
-	        median = (long) values[values.length / 2];
+	static long readAndStore(TestURL test, int runId) throws Exception {
+		File testOutput = new File(OUTPUT_LOCATION, test.getId());
+		if (testOutput.exists()) {
+			testOutput.delete();
+			testOutput.mkdir();
 		}
-	    return median;
+		long l = System.currentTimeMillis();	
+		URL nocacheUrl = new URL(test.getUrl().toString()+"?timestamp="+l);	
+		try (FileOutputStream fileOutputStream = new FileOutputStream(new File(testOutput, test.getId()+"_"+runId));
+				FileChannel fileChannel = fileOutputStream.getChannel();
+				ReadableByteChannel readableByteChannel = Channels.newChannel(nocacheUrl.openStream());) {
+			fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+		}
+		catch(java.net.SocketException e) {
+			System.err.println("Socket exception: " + e);
+		}
+		return (System.currentTimeMillis()-l);
 	}
 
-    public String toString() {
-    	return id+":"+Arrays.toString(results);
-    }
-    
-    public class TestStatistics {
-    	private long count;
-    	private long sum;
-    	private long min;
-    	private long max;
-    	private long average;
-    	private long median;
-    	private long stderr;
-
-    	public long getCount() {
-			return count;
+	static long readAndForget(TestURL test, int runId) throws Exception {
+		long l = System.currentTimeMillis();	
+		URL nocacheUrl = new URL(test.getUrl().toString()+"?timestamp="+l);	
+		try (ReadableByteChannel readableByteChannel = Channels.newChannel(nocacheUrl.openStream())) {
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			while (readableByteChannel.read(buffer) != -1) {
+				buffer.clear();
+			}
 		}
-
-		public long getSum() {
-			return sum;
+		catch(java.net.SocketException e) {
+			System.err.println("Socket exception: " + e);
 		}
+		return (System.currentTimeMillis()-l);
+	}
 
-		public long getMin() {
-			return min;
+	static void exporCSV(String filename, TestSuite testSuite) throws Exception {
+		String DEL = ";";
+
+		try (FileOutputStream fileOutputStream = new FileOutputStream(
+				new File(OUTPUT_LOCATION, filename))) {
+
+			int trials = 0;
+			String header = "Trial ID"+DEL;
+			for (TestURL testUrl: testSuite.getTests()) {
+				trials = testUrl.getResults().size();
+				header += testUrl.getId()+DEL;
+			}
+			header += "\n";
+			fileOutputStream.write(header.getBytes());
+
+			String s = "";
+			for (int i=0; i<trials; i++) {
+				s += "T"+(i+1)+DEL;
+				for (TestURL testUrl: testSuite.getTests()) {
+					s += testUrl.getResult(i)+DEL;
+				}
+				s += "\n";
+			}
+			fileOutputStream.write(s.getBytes());
 		}
+	}
 
-		public long getMax() {
-			return max;
+	static void exporSummaryCSV(String filename, TestSuite testSuite) throws Exception {
+		String DEL = ";";
+
+		try (FileOutputStream fileOutputStream = new FileOutputStream(
+				new File(OUTPUT_LOCATION, filename))) {
+
+			String header = "ID"+DEL+"CNT"+DEL+"SUM"+DEL+"MIN"+DEL+"MAX"+DEL+"AVG"+DEL+"MED"+DEL+"STD"+"\n";
+			fileOutputStream.write(header.getBytes());
+
+			for (TestURL t : testSuite.getTests()) {
+				TestStatistics ts = t.getTestStatistics();
+				String s = "";
+				s += t.getId()+DEL;
+				s += ts.getCount()+DEL;
+				s += ts.getSum()+DEL;
+				s += ts.getMin()+DEL;
+				s += ts.getMax()+DEL;
+				s += ts.getAverage()+DEL;
+				s += ts.getMedian()+DEL;
+				s += ts.getStderr()+DEL;
+				s += "\n";
+				fileOutputStream.write(s.getBytes());
+			}
+			
 		}
-
-		public long getAverage() {
-			return average;
-		}
-
-		public long getMedian() {
-			return median;
-		}
-
-		public long getStderr() {
-			return stderr;
-		}
-
-    	public String toString() {
-    		String s = id+"\n";
-    		s += "  CNT: "+count+"\n";
-    		s += "  SUM: "+sum+"\n";
-    		s += "  MIN: "+min+"\n";
-    		s += "  MAX: "+max+"\n";
-    		s += "  AVG: "+average+"\n";
-    		s += "  MED: "+median+"\n";
-    		s += "  STD: "+stderr;
-    		return s;
-    	}
-    }
+	}
 }
